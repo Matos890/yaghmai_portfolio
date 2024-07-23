47392ad2f1b70f867a3f352b00192d3e64222408
Matos890 <matosyaghmai@gmail.com>
2024-07-23 13:49:36 +0200

    'animationCompleted'

--- public/js/index.js
+++ public/js/index.js
@@ -50,6 +50,8 @@ document.querySelector(".nav__links").addEventListener("click", function (e) {
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
   }
 });
+const screenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
 ////////////////////////////////////////////////////////////
 /////////////////ANIMATION/////////////////////////
 /////////////////////////////////////////////////
@@ -397,84 +399,220 @@ sections.forEach((section) => {
   });
 });
 ////////////////ANIMAZIONE ALTERNATIVA
-const rectangle1 = document.querySelector(".rectangle1");
-const rectangle1dimension = rectangle1.getBoundingClientRect()
-console.log('ei', rectangle1dimension.width)
-const rectangle2dimension = rectangle1.getBoundingClientRect()
-const rectangle2 = document.querySelector(".rectangle2");
-const fakeRectangle = document.querySelector(".fakeRectangle");
-const fakeDimension = fakeRectangle.getBoundingClientRect();
-const yaghmai = document.querySelector(".yaghmai");
-const yaghmaiDimension = yaghmai.getBoundingClientRect();
-const mattia = document.querySelector(".mattia");
-const mattiaDimension = mattia.getBoundingClientRect();
-const wrapperTextHero = document.querySelector('.text')
-const wrapperTextDimension = wrapperTextHero.getBoundingClientRect()
-// gsap.set(mattia, {
-//   x: () => mattiaDimension.width,
-// // });
-// gsap.set(yaghmai, {
-//   x: () => -yaghmaiDimension.width,
-// });
-gsap.set(rectangle1, {
-  x:()=>-rectangle1dimension.width,
-});
-gsap.set(wrapperTextHero, {
-  y:()=> - wrapperTextDimension.height - 100
-})
-gsap.set(rectangle2, {
-  x:()=>rectangle1dimension.width,
-});
-gsap.set(fakeRectangle, {
-  transformOrigin: '50% 50%',
-})
 const tl1 = gsap.timeline();
-tl1.addLabel('primaReveal')
-tl1.from(fakeRectangle, {
-  scale:0,
-})
-tl1.from(fakeRectangle, {
-  // width: () => fakeDimension.height * 2,
+const tl2 = gsap.timeline();
+const rectangle1 = document.querySelector(".rectangle1");
+let rectangle1dimension = window.getComputedStyle(rectangle1);
+console.log("rectangle1dimension", rectangle1dimension);
+
+const rectangle2 = document.querySelector(".rectangle2");
+let rectangle2dimension = window.getComputedStyle(rectangle2);
+
+const fakeRectangle = document.querySelector(".fakeRectangle");
+const fakeDimension = window.getComputedStyle(fakeRectangle);
+
+const yaghmai = document.querySelector(".yaghmai");
+const yaghmaiDimension = window.getComputedStyle(yaghmai);
+
+const mattia = document.querySelector(".mattia");
+const mattiaDimension = window.getComputedStyle(mattia);
+
+const wrapperTextHero = document.querySelector(".text");
+const wrapperTextDimension = window.getComputedStyle(wrapperTextHero);
+
+const wrapperGrid = document.querySelector(".wrapperGrid");
+const wrapperGridDimension = window.getComputedStyle(wrapperGrid);
+
+const initializeAnimation = function () {
+  const screenWidth = window.innerWidth;  // Aggiungi questa linea
+
+  gsap.set(rectangle1, {
+    x: () => -parseFloat(rectangle1dimension.width) ,
+  });
+  gsap.set(wrapperTextHero, {
+    y: () => -parseFloat(wrapperTextDimension.height) - 100,
+  });
+  gsap.set(rectangle2, {
+    x: () => parseFloat(rectangle2dimension.width) ,
+  });
+  gsap.set(fakeRectangle, {
+    transformOrigin: "50% 50%",
+  });
+
+  tl1.addLabel("primaReveal");
+  tl1.from(fakeRectangle, {
+    scale: 0,
+  });
+  tl1.from(
+    fakeRectangle,
+    {
+      y: () => parseFloat(fakeDimension.height) / 2,
+      height: () => parseFloat(fakeDimension.height) * 2 + 1,
+      rotation: 90,
+      delay: 1,
+    },
+    "primaReveal"
+  );
+  tl1.from(
+    mattia,
+    {
+      x: () => parseFloat(mattiaDimension.width) + 200,
+      delay: 1,
+    },
+    "primaReveal"
+  );
+  tl1.from(
+    yaghmai,
+    {
+      x: () => -parseFloat(yaghmaiDimension.width) -  200,
+      delay: 1,
+    },
+    "primaReveal"
+  );
+  tl1.addLabel("secondReveal");
+  tl1.to(
+    rectangle1,
+    {
+      opacity: 1,
+      duration: 0.4,
+      x: () => 0,
+    },
+    "secondReveal"
+  );
+  tl1.to(
+    rectangle2,
+    {
+      opacity: 1,
+      duration: 0.4,
+      x: () => 0,
+    },
+    "secondReveal"
+  );
+  tl1.to(fakeRectangle, {
+    opacity:0,
+    duration:0.4
+  }, "secondReveal")
+  tl1.to(wrapperTextHero, {
+    y: () => 0,
+  });
   
-  y:() => fakeDimension.height / 2,
-  height: () =>fakeDimension.height * 2,
-  rotation: 90,
-  delay: 1,
-}, 'primaReveal');
-// tl1.to(fakeRectangle, {
-//   width:() => -100,
-//   delay:1
-// }, 'primaReveal')
-tl1.from(mattia, {
-  x: () => mattiaDimension.width,
-  delay:1
 
-}, 'primaReveal')
-tl1.from(yaghmai, {
+  if (screenWidth <= 900) {
+    tl1.kill();
+    tl2.set(fakeRectangle, {
+      opacity: 0,
+    });
+    tl2.set(mattia, {
+      opacity: 1,
+      y: () => -parseFloat(mattiaDimension.height) * 1.5,
+      x: 0,
+    });
+    tl2.set(yaghmai, {
+      y: () => parseFloat(yaghmaiDimension.height) * 1.5,
+      x: 0,
+    });
+    tl2.set(rectangle1, {
+      width: () => screenWidth / 3,
+      y: () => parseFloat(wrapperGridDimension.height) / 4,
+      transformOrigin: "50% 50%",
+      rotate: 220,
+      opacity: 0,
+    });
+    tl2.set(rectangle2, {
+      width: () => screenWidth / 3,
+      transformOrigin: "50% 50%",
+      y: () => -parseFloat(wrapperGridDimension.height) / 5,
+      rotate: 220,
+      opacity: 0,
+    });
+    tl2.addLabel("revealSmall1");
+    tl2.to(
+      rectangle1,
+      {
+        rotate: 0,
+        opacity: 1,
+        width: () => screenWidth / 1.5,
+      },
+      "revealSmall1"
+    );
+    tl2.to(
+      rectangle2,
+      {
+        rotate: 0,
+        opacity: 1,
+        width: () => screenWidth / 1.5,
+      },
+      "revealSmall1"
+    );
+    tl2.addLabel("revealSmall2");
+    tl2.to(
+      rectangle1,
+      {
+        y: () => 0,
+      },
+      "revealSmall2"
+    );
+    tl2.to(
+      rectangle2,
+      {
+        y: () => 0,
+      },
+      "revealSmall2"
+    );
+    tl2.to(
+      mattia,
+      {
+        y: () => 0,
+      },
+      "revealSmall2"
+    );
+    tl2.to(
+      yaghmai,
+      {
+        y: () => 0,
+      },
+      "revealSmall2"
+    );
+    tl2.to(
+      wrapperTextHero,
+      {
+        y: 0,
+      },
+      "revealSmall2"
+    );
+  }
+};
 
-  x: () => -yaghmaiDimension.width,
-  delay:1
-}, 'primaReveal')
-tl1.addLabel('secondReveal')
-tl1.to(rectangle1, {
-  opacity:1,
-  duration:0.4,
-x:()=>0 
-}, 'secondReveal-=0.35')
+const debounce = (func, delay) => {
+  let timeout;
+  return function (...args) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
 
-tl1.to(rectangle2, {
-  opacity:1,
-  duration:0.4,
-x:() =>0
-}, 'secondReveal-=0.35')
-tl1.to(fakeRectangle, {
-  opacity: 0,
-  duration:0.5,
-} )
-tl1.to(wrapperTextHero, {
-  
-  y:()=>
-   0  
-  
-})
+window.addEventListener(
+  "resize",
+  debounce(() => {
+    tl1.progress(1);
+    tl2.progress(1);
+    tl1.kill();
+    tl2.kill();
+    gsap.killTweensOf(tl1);
+    gsap.killTweensOf(tl2);
 
+    // Resetta gli stili degli elementi
+    const elementsToReset = [
+      mattia, yaghmai, fakeRectangle, wrapperGrid, wrapperTextHero, rectangle1, rectangle2
+    ];
+    elementsToReset.forEach(element => {
+      element.removeAttribute('style');
+    });
+
+    initializeAnimation();
+  }, 300)
+);
+
+initializeAnimation();
