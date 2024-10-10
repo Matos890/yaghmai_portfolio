92377e88975ef427cb73438c1512480c08b7df67
Matos890 <matosyaghmai@gmail.com>
2024-10-10 13:31:21 +0200

    'parcelIssue1'

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const dotenv = require("dotenv").config();
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src"));
 
 // Configurare la directory dei file statici
 const parts = [__dirname, 'public'];
