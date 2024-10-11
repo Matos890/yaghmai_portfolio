80e11285a393b02a8c189a34d91e85822d3fcb62
Matos890 <matosyaghmai@gmail.com>
2024-10-11 13:51:29 +0200

    'ParcelWorksWithaWorkAround'

--- app.js
+++ app.js
@@ -3,19 +3,16 @@ const path = require("path");
 const viewRoute = require("./routes/viewRoute");
 const dotenv = require("dotenv").config({ path: path.join(__dirname, "config.env") });
 
-
 const app = express();
 
-// app.set("views", path.join(__dirname, "build"));
+app.use("/", viewRoute);
 
 // Configurare la directory dei file statici
 app.use(express.static(path.join(__dirname, "build")));
 
 // Utilizzare la rotta definita
-
-app.use("/", viewRoute);
-
-// Avviare il server sulla porta 2000
+// Definire la rotta per la root prima del middleware statico
+// Avviare il server sulla porta 5000
 app.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
+    console.log("Server is running on http://localhost:5000");
 });
