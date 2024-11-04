3067f1f933312617536cb5ce81a9a3bfce727741
Matos890 <matosyaghmai@gmail.com>
2024-11-04 16:42:28 +0100

    'parcelIssues1'

--- public/js/indexTelegrafo.js
+++ public/js/indexTelegrafo.js
@@ -1,38 +1,13 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 import { Timeline } from "gsap/gsap-core";
-import cover from "canvas-image-cover";
-
-// Importa le immagini
-import emblemataBookmark1 from "../img/Emblemata_1624_bookmark_1.jpg";
-import chappeBackground from "../img/chappebackground.png";
-import franklinBookmark2 from "../img/Franklin_hand_bookmark_2.jpg";
-import magnetism1Background from "../img/magnetism1background.png";
-import faraday from "../img/Faraday.png";
-import henry from "../img/henry.png";
-import railwayMap from "../img/railwaymap.png";
-import magnetismBackground2 from "../img/magnetismbackground2.png";
-import giornalistiBookmark3 from "../img/giornalisti_bookmark_3.png";
-import morseBackground from "../img/morsebackground.png";
-import morse from "../img/morse.png";
-import testBackground from "../img/testbackground.png";
-import morseNotes from "../img/morseNotes.jpg";
-import giornalistiBackground from "../img/giornalistibackground.png";
-import submarineBookmark4 from "../img/submarine_bookmark_4.jpg";
-import shipBackground from "../img/shipbackground.png";
-import cyrus from "../img/cyrus.png";
-import messageQueen from "../img/messagequeen.png";
-import telegraphLines1871 from "../img/1871Telegraph lines.jpg";
-import telegraphLines1880 from "../img/1880Telegraph lines.jpg";
-import telegraphLines1901 from "../img/1901Telegraph lines.jpg";
-import caricatureBackground from "../img/caricaturebackground.png";
 
 gsap.registerPlugin(ScrollTrigger);
-
+const cover = require("canvas-image-cover");
 const canvas = document.getElementById("myCanvas");
 const canvas1 = document.getElementById("myCanvas1");
 const frame = document.querySelectorAll("[class^='frame']");
-const body = document.querySelector('body');
+const body = document.querySelector('body')
 const bookmarkFrame = document.querySelectorAll("[class^='bookmarkFrame']");
 const frame1 = document.querySelector(".frame1");
 const sectionz = document.querySelectorAll("section");
@@ -43,36 +18,34 @@ const mainWrapper = document.querySelector(".mainWrapper");
 const whiteSpace = document.querySelectorAll(".whiteSpace");
 const ctx1 = canvas1.getContext("2d");
 const scroller = document.querySelectorAll(".scroller");
-
 ctx.globalCompositeOperation = "source-over";
 bookmarkFrame.forEach((bookmark) => {
   console.log(bookmark);
 });
-
-// IMAGES
+//IMAGES
 const imageSources = [
-  emblemataBookmark1,
-  chappeBackground,
-  franklinBookmark2,
-  magnetism1Background,
-  faraday,
-  henry,
-  railwayMap,
-  magnetismBackground2,
-  giornalistiBookmark3,
-  morseBackground,
-  morse,
-  testBackground,
-  morseNotes,
-  giornalistiBackground,
-  submarineBookmark4,
-  shipBackground,
-  cyrus,
-  messageQueen,
-  telegraphLines1871,
-  telegraphLines1880,
-  telegraphLines1901,
-  caricatureBackground,
+  "./img/Emblemata_1624_bookmark_1.jpg",
+  "./img/chappebackground.png",
+  "./img/Franklin_hand_bookmark_2.jpg",
+  "./img/magnetism1background.png",
+  "./img/Faraday.png",
+  "./img/henry.png",
+  "./img/railwaymap.png",
+  "./img/magnetismbackground2.png",
+  "./img/giornalisti_bookmark_3.png",
+  "./img/morsebackground.png",
+  "./img/morse.png",
+  "./img/testbackground.png",
+  "./img/morseNotes.jpg",
+  "./img/giornalistibackground.png",
+  "./img/submarine_bookmark_4.jpg",
+  "./img/shipbackground.png",
+  "./img/cyrus.png",
+  "./img/messagequeen.png",
+  "./img/1871Telegraph lines.jpg",
+  "./img/1880Telegraph lines.jpg",
+  "./img/1901Telegraph lines.jpg",
+  "./img/caricaturebackground.png",
 ];
 
 const images = [];
@@ -97,10 +70,8 @@ function preloadImages(sources, callback) {
     }
   });
 }
-
 console.log("this is bookmark", bookmark);
 console.log("this is image", images);
-
 function getDimension() {
   let canvasX = canvas.getBoundingClientRect().left;
   let canvasY = canvas.getBoundingClientRect().top;
@@ -138,9 +109,9 @@ function getDimension() {
     frameHeightB,
   };
 }
-
 function bookmarkPos(bookmark, frameBX, frameBY, frameWidthB, frameHeightB) {
   //1.
+
   cover(bookmark[0], frameBX[0], frameBY[0], frameWidthB[0], frameHeightB[0], {
     mode: "cover",
   })
@@ -155,6 +126,7 @@ function bookmarkPos(bookmark, frameBX, frameBY, frameWidthB, frameHeightB) {
     .pan(0.2, 0)
     .render(ctx);
   //3.
+
   cover(bookmark[2], frameBX[2], frameBY[2], frameWidthB[2], frameHeightB[2], {
     mode: "cover",
   })
@@ -162,6 +134,7 @@ function bookmarkPos(bookmark, frameBX, frameBY, frameWidthB, frameHeightB) {
     .pan(0.295, 0)
     .render(ctx);
   //4.
+
   cover(bookmark[3], frameBX[3], frameBY[3], frameWidthB[3], frameHeightB[3], {
     mode: "cover",
   })
@@ -171,69 +144,77 @@ function bookmarkPos(bookmark, frameBX, frameBY, frameWidthB, frameHeightB) {
 }
 
 function draw() {
-  const screenWidth = window.innerWidth;
-  const screenHeight = window.innerHeight;
+  {
+    const screenWidth = window.innerWidth;
+    const screenHeight = window.innerHeight;
 
-  canvas.setAttribute("width", screenWidth);
-  canvas.setAttribute("height", screenHeight);
-  canvas1.setAttribute("width", screenWidth);
-  canvas1.setAttribute("height", screenHeight);
+    canvas.setAttribute("width", screenWidth);
+    canvas.setAttribute("height", screenHeight);
+    canvas1.setAttribute("width", screenWidth);
+    canvas1.setAttribute("height", screenHeight);
 
-  let totalWidth = 0;
-  scroller.forEach((section) => {
-    totalWidth += section.offsetHeight;
-  });
-
-  let ratioWH = coverArtSection.offsetWidth / coverArtSection.offsetHeight;
-  console.log(ratioWH);
-  totalWidth =
-    (mainWrapper.offsetWidth / ratioWH) + (coverArtSection.offsetHeight * 3) - coverArtSection.offsetHeight;
-  document.body.style.height = totalWidth + 'px';
-  console.log("width of sectionz", totalWidth);
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
-  const {
-    frameX,
-    frameY,
-    frameWidth,
-    frameHeight,
-    frameBX,
-    frameBY,
-    frameWidthB,
-    frameHeightB,
-  } = getDimension();
-  bookmarkPos(bookmark, frameBX, frameBY, frameWidthB, frameHeightB);
-  images.forEach((imgObj, i) => {
-    if (imgObj.src.includes("background")) {
-      ctx1.globalAlpha = 0.2;
-      ctx1.drawImage(
-        imgObj,
-        frameX[i],
-        frameY[i],
-        frameWidth[i],
-        frameHeight[i]
-      );
-    } else {
-      ctx.shadowColor = "rgba(0, 0, 0, 0.4)";
-      ctx.shadowBlur = 7;
-      ctx.shadowOffsetX = 10;
-      ctx.shadowOffsetY = 10;
-      ctx.drawImage(
-        imgObj,
-        frameX[i],
-        frameY[i],
-        frameWidth[i],
-        frameHeight[i]
-      );
-    }
-  });
+    let totalWidth = 0;
+    scroller.forEach((section) => {
+      totalWidth += section.offsetHeight;
+    });
+let ratioWH = coverArtSection.offsetWidth/coverArtSection.offsetHeight;
+console.log(ratioWH)
+    totalWidth =
+      
+      (mainWrapper.offsetWidth/ratioWH) + (coverArtSection.offsetHeight * 3) - (coverArtSection.offsetHeight  );
+      document.body.style.height = totalWidth + 'px';
+      // body.setAttribute('height', totalWidth)
+    console.log("width of sectionz", totalWidth);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+    const {
+      frameX,
+      frameY,
+      frameWidth,
+      frameHeight,
+      frameBX,
+      frameBY,
+      frameWidthB,
+      frameHeightB,
+    } = getDimension();
+    bookmarkPos(bookmark, frameBX, frameBY, frameWidthB, frameHeightB);
+    images.forEach((imgObj, i) => {
+      if (imgObj.src.includes("background")) {
+        ctx1.globalAlpha = 0.2;
+        ctx1.drawImage(
+          imgObj,
+          frameX[i],
+          frameY[i],
+          frameWidth[i],
+          frameHeight[i]
+        );
+        // } else if (imgObj.src.includes("bookmark")) {
+        //   cover(imgObj, frameX[i], frameY[i], frameWidth[i], frameHeight[i], {
+        //     mode: "cover",
+        //   })
+        //     .zoom(1.5)
+        //     .pan(0.7, 0)
+        //     .render(ctx);}
+      } else {
+        ctx.shadowColor = "rgba(0, 0, 0, 0.4)";
+        ctx.shadowBlur = 7;
+        ctx.shadowOffsetX = 10;
+        ctx.shadowOffsetY = 10;
+        ctx.drawImage(
+          imgObj,
+          frameX[i],
+          frameY[i],
+          frameWidth[i],
+          frameHeight[i]
+        );
+      }
+    });
+  }
 }
-
 function updateCanvas() {
   draw();
   requestAnimationFrame(updateCanvas);
 }
-
 function calculatePositionsGif() {
   const container1 = document.querySelector(".containerFrame2");
   const positionContainer1X = container1.getBoundingClientRect().left;
@@ -254,10 +235,228 @@ function calculatePositionsGif() {
     distance,
   };
 }
-
 preloadImages(imageSources, () => {
   window.addEventListener("resize", draw);
   window.addEventListener("load", draw);
-  let { positionContainer1X, gifX, positionContainer2X, endTrigger, distance } = calculatePositionsGif();
-  updateCanvas();
+  let { positionContainer1X, gifX, positionContainer2X, endTrigger, distance } =
+    calculatePositionsGif();
+
+  window.addEventListener("resize", () => {
+    ({ positionContainer1X, gifX, positionContainer2X, endTrigger, distance } =
+      calculatePositionsGif());
+  });
+  ///OPENING
+  const tlOpening = gsap.timeline();
+  tlOpening.add("diagonali");
+  tlOpening.addLabel("redTel");
+  tlOpening
+    .from(
+      ".imgDiag1",
+      {
+        x: 2000,
+        duration: 5,
+        ease: "power1.inOut",
+        filter: "blur(3px)",
+      },
+      "diagonali"
+    )
+    .from(
+      ".imgDiag2",
+      {
+        x: 2000,
+        duration: 5,
+        filter: "blur(3px)",
+      },
+      "diagonali"
+    )
+    .from(
+      ".imgRed",
+      {
+        duration: 2,
+        ease: "circ.in",
+        scale: 0,
+      },
+      "diagonali"
+    )
+    .from(
+      ".titleContainer",
+      {
+        x: 300,
+        opacity: 0,
+        duration: 3,
+        ease: "power3.out",
+      },
+      "diagonali+=2"
+    )
+    .from(
+      ".squareContainer",
+      {
+        x: 200,
+        opacity: 0,
+        ease: "power3.out",
+      },
+      "diagonali+=2"
+    )
+    .from(
+      ".torpedoFish",
+      {
+        x: 200,
+        duration: 3,
+        opacity: 0,
+      },
+      "diagonali+=2"
+    )
+    .from(
+      ".naveContainer",
+      {
+        x: 1200,
+        duration: 4,
+        ease: "power3.inOut",
+      },
+      "diagonali"
+    )
+    .from(
+      ".telegraphImg",
+      {
+        duration: 2,
+        ease: "circ.in",
+        // scale:0,
+        opacity: 0,
+      },
+      "diagonali+=3"
+    );
+
+  //MAIN SECTIONS
+  gsap.from(".titoloChappe", {
+    opacity: 0,
+    y: "3vh",
+    duration: 1,
+    ease: "power1.inOut",
+  });
+  gsap.from(".spanBookmark", {
+    opacity: 0,
+    y: "10vh",
+    stagger: 0.2,
+    duration: 5,
+  });
+  const sections = gsap.utils.toArray(".mainWrapper .scroller ");
+  let scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    duration: 5,
+    delay: 0.2,
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".sectioni",
+      // markers:true,
+      pin: true,
+      scrub: 5,
+      pinSpacing: false,
+      invalidateOnRefresh: true,
+      start: "center center",
+      end:()=> `+=${(mainWrapper.offsetWidth/(coverArtSection.offsetWidth/coverArtSection.offsetHeight))}`,
+      onUpdate: () => {
+        requestAnimationFrame(draw);
+      },
+    },
+  });
+  const gifMoving = function () {
+    gsap.to(".gif1", {
+      x: () => {
+        const container1 = document.querySelector(".containerFrame2");
+        const positionContainer1X = container1.getBoundingClientRect().left;
+        const canvasPositionX = canvas.getBoundingClientRect().left;
+        const gifX = positionContainer1X - canvasPositionX;
+
+        const container2 = document.querySelector(".containerFrame2Copy");
+        const positionContainer2X = container2.getBoundingClientRect().left;
+        const endTrigger = positionContainer2X - canvasPositionX;
+
+        const distance = endTrigger - gifX;
+        return distance;
+      },
+      ease: "power1.inOut",
+      scrollTrigger: {
+        trigger: ".gif1",
+        // endTrigger:'containerFrame2Copy',
+        containerAnimation: scrollTween,
+        horizontal: true,
+        pin: true,
+        scrub: true,
+        pinType: "transform",
+        pinSpacing: false,
+        invalidateOnRefresh: true,
+        start: () => `0   ${gifX}`,
+        end: () => `center  -${endTrigger}`,
+      },
+    });
+
+    //-- MAP ANIMATION //
+    const tl = gsap.timeline();
+
+    tl.add("start")
+      .from(
+        map,
+        {
+          opacity: 0,
+          duration: 2,
+        },
+        "start"
+      )
+      .from(
+        pallini,
+        {
+          opacity: 0,
+          duration: 2,
+        },
+        "start"
+      )
+      .from(cities, { opacity: 0, duration: 2 })
+      .from(primaEra, { opacity: 0, duration: 2 })
+      .from(secondaEra, { opacity: 0, duration: 2 })
+      .from(terzaEra, { opacity: 0, duration: 2 })
+      .from(after1830, {
+        opacity: 0,
+        duration: 2,
+      });
+    ScrollTrigger.create({
+      animation: tl, // La timeline da collegare
+      containerAnimation: scrollTween,
+      trigger: ".p-03", // L'elemento che fa scattare lo ScrollTrigger
+      start: "left center",
+
+      end: "+=200", // Quando deve iniziare l'animazione
+      // markers: true  // Mostrare i markers per il debug
+    });
+
+    const tl1 = gsap.timeline();
+    const ages = gsap.utils.toArray(".ageMap ");
+    const animationAges = tl1
+      .from(ages, {
+        y: -50,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.7,
+        ease: "power3.inOut",
+      })
+      .to(ages, {
+        x: -50,
+        duration: 1,
+        stagger: 0.7,
+        ease: "power3.inOut",
+      });
+
+    ScrollTrigger.create({
+      animation: animationAges, // La timeline da collegare
+      containerAnimation: scrollTween,
+      pinSpacing: false,
+      trigger: ".mapdiff", // L'elemento che fa scattare lo ScrollTrigger
+      start: "left center",
+      // markers: true,
+      end: "+=200", // Quando deve iniziare l'animazione
+      // markers: true  // Mostrare i markers per il debug
+    });
+  };
+
+  window.addEventListener("resize", gifMoving());
+  requestAnimationFrame(updateCanvas);
 });
